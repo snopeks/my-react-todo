f93fb94ff6490649a938b32abf59c0bd68809380
snopeks <creativesnopeks@gmail.com>
2017-10-25 14:56:40 -0700

    complete sprint2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import './index.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        {this.props.children}
       </div>
     );
   }
