45ce62f4537598f6e11b35ec978ae9cb5de45d97
snopeks <creativesnopeks@gmail.com>
2017-10-29 17:35:26 -0700

    add delete method to container

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -34,13 +34,24 @@ class TodosContainer extends Component{
     })
     console.log('creating todo', newBody)
   }
+  deleteTodo(todo){
+    console.log('deleting todo', todo)
+    TodoModel.delete(todo).then((res)=> {
+      let todos = this.state.todos.filter(function(todo){
+        return todo._id !== res._id
+      });
+      this.setState({todos})
+    })
+  }
+
   render(){
     return(
       <div className = 'todosContainer'>
         <CreateTodoForm
           createTodo={this.createTodo.bind(this)} />
         <TodoList
-          todos={this.state.todos} />
+          todos={this.state.todos}
+          onDeleteTodo={this.deleteTodo.bind(this)} />
       </div>
     )
   }
